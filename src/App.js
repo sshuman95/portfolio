84f9680baef4fbc3321ff253ed1139c536cc09ca
unknown <sidney.shuman95@gmail.com>
2020-04-15 12:37:10 -0500

    starting the homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpg';
+import newLogo from './new_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='mainHead'>
+      <hgroup className='header'>
+        <figure>
+            <img src={newLogo}/>
+          </figure>
+        <h3>Web Developer - Portfolio</h3>
+      </hgroup>
+      <nav className='headNav'>
+        <ul>
+          <li>Github</li>
+          <li>LinkedIn</li>
+          <li>third</li>
+        </ul>
+      </nav>
+      <section className='headSec'>
+        <article>
+          <figure>
+            <img src={logo} className='me'/>
+          </figure>
+          <h3>About Me</h3>
+          <p>My name is Sidney Shuman. I am web developer located in Richmond, Virginia. I'm skilled in HTML, CSS, JS, React, Node, and Express. Feel free to browse the projects I've worked on below. </p>
+        </article>
+      </section>
       </header>
+      <main>
+        <section className='section1'>
+          <article>
+            <header>
+              <h3>KJPresley Designs</h3>
+              <p><a href='http://www.google.com' target='_blank' rel="noopener noreferrer">Github</a> | <a href='google.com' rel="noopener noreferrer" target='_blank'>Live</a></p>
+            </header>
+            <p>Any thoughts on this?</p>
+          </article>
+        </section>
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
